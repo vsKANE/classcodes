68a606948080d473588d89728ef4d21fee33a481
vsKANE <cyrilnorway@outlook.com>
2025-05-21 08:16:34 -0800

    Update script.js
    added javascript for step-by-step form

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+function nextStep(stepId) {
+    document.querySelectorAll("form > div").forEach(div => div.style.display = "none");
+    document.getElementById(stepId).style.display = "block";
+}
